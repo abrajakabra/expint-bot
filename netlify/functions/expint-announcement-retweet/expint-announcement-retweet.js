110a1c8c984d2efe327f6c016facff5ad1ed3dbf
abrajakabra <jan.brand@ddfakt.de>
2022-06-22 11:22:17 +0200

    Update expint-announcement-retweet.js

--- netlify/functions/expint-announcement-retweet/expint-announcement-retweet.js
+++ netlify/functions/expint-announcement-retweet/expint-announcement-retweet.js
@@ -1,7 +1,7 @@
 const { schedule } = require("@netlify/functions");
 const { TwitterApi } = require("twitter-api-v2");
 
-module.exports.handler = schedule("0 12,15,18 * * 3", async (event) => {
+module.exports.handler = schedule("0 10,13,16 * * 3", async (event) => {
   const client = new TwitterApi({
     appKey: process.env.TWITTER_APP_KEY,
     appSecret: process.env.TWITTER_APP_SECRET,
